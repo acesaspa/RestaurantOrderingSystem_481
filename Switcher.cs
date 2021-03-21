703f30cad39c7e5ce868ccec4e4aff505f4a1a45
ronan <ronan.ranknemish1@ucalgary.ca>
2021-03-21 16:54:12 -0600

    Main Windows to User Controls

---
+++ Switcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantOrderingSystem_481
+{
+    class Switcher
+    {
+        public static MainWindow pageSwitcher;
+
+        public static void Switch(UserControl newPage)
+        {
+
+        }
+    }
+}
--- UserControl1.xaml.cs
+++ UserControl1.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,20 +14,13 @@ using System.Windows.Shapes;
 namespace RestaurantOrderingSystem_481
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserControl1 : UserControl
     {
-        public MainWindow()
+        public UserControl1()
         {
             InitializeComponent();
-            Switcher.pageSwitcher = this;
-            Switcher.Switch(new MainMenu());
-        }
-
-        public void Navigate(UserControl nextPage)
-        {
-            this.Content = nextPage;
         }
     }
 }
