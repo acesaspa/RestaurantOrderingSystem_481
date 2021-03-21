f73dc115c1bfdaeb3f717a622881213a5aa6385d
ronan <ronan.ranknemish1@ucalgary.ca>
2021-03-21 16:33:33 -0600

    Added Rest of files

---
+++ MenuFilter2.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestaurantOrderingSystem_481
+{
+    /// <summary>
+    /// Interaction logic for MenuFilter2.xaml
+    /// </summary>
+    public partial class MenuFilter2 : Window
+    {
+        public MenuFilter2()
+        {
+            InitializeComponent();
+        }
+    }
+}
