1e5698c569c018d3bcbc2fe7ae9761ed5583a710
mehradFaridan <60161048+mehradFaridan@users.noreply.github.com>
2021-03-26 16:05:56 -0600

    change

--- MenuItem.xaml.cs
+++ MenuItem.xaml.cs
@@ -18,7 +18,37 @@ namespace RestaurantOrderingSystem_481
     /// </summary>
     public partial class MenuItem : UserControl
     {
+
+        /***************************
+         
+            Variable Declarations:
+         
+        ***************************/
+
+        // Item Name
         public string item_name;
+
+        // Item Description
+        public string item_description;
+
+        // Item Characteristics
+        public bool item_spicy;
+        public bool item_peanut;
+        public bool item_gluten;
+        public bool item_vegetarian;
+        public bool item_vegan;
+        public bool item_halal;
+        public bool item_koshur;
+
+
+
+        /***************************
+
+             Getters & Setters:
+
+        ***************************/
+
+        // Item Name
         public string Item_name
         {
             get { return item_name; }
@@ -29,17 +59,92 @@ namespace RestaurantOrderingSystem_481
             }
         }
 
-        public string item_description;
 
+        // Item Description
         public string Item_description
         {
             get { return item_description; }
+            
             set
             {
                 item_description = value;
                 this.ItemDescription.Content = this.item_description;
             }
+        
         }
+
+
+        // Item Characteristics:
+        public bool Item_spicy
+        {
+
+            get { return item_spicy; }
+
+            set { item_spicy = false;}
+
+        }
+
+        public bool Item_peanut
+        {
+
+            get { return item_peanut; }
+
+            set{ item_peanut = false; }
+
+        }
+
+        public bool Item_gluten
+        {
+
+            get { return item_gluten; }
+
+            set { item_gluten = false; }
+
+        }
+
+        public bool Item_vegetarian
+        {
+
+            get { return item_vegetarian; }
+
+            set { item_vegetarian = false; }
+
+        }
+
+        public bool Item_vegan
+        {
+
+            get { return item_vegan; }
+
+            set { item_vegan = false; }
+
+        }
+
+        public bool Item_halal
+        {
+
+            get { return item_halal; }
+
+            set { item_halal = false; }
+
+        }
+
+        public bool Item_koshur
+        {
+
+            get { return item_koshur; }
+
+            set { item_koshur = false; }
+
+        }
+
+
+
+
+
+
+
+
 
 
         public MenuItem()
